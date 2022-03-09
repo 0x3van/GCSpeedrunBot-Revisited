067e68a10fe0bd251bd91ebe3eed444e8f82add6
evan <97516411+evannnns@users.noreply.github.com>
2022-03-09 09:32:38 +0800

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,48 @@
+
+local modules = {
+    require "httprequest"
+    require "drawing"
+    require "axios"
+    require "discordia"
+}
+-- Color3.fromRGB(179, 125, 125)
+local c = Color3.new(math.rand*(color3(math.rand, math.rand, math.rand,math.rand*(color3(math.rand, math.rand, math.rand,math.rand*(color3(math.rand, math.rand, math.rand)))()
+-- Color3 runs by 3 ops wtfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff nigger
+function rainbowchange1(c) -- math.rand color ig :brocken:
+    discordia.Avatar.ProfileBanner = RemoteEvent.new("firecolor") -- event handling for colors
+    firecolor.httprequest = httprequest.json:encode() 
+    local headers = {
+        targeturl: "https://www.discord.com/api/v1/colorbanner/changecolor.aspx",
+        "headers": {
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "no-cache",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "pragma": "no-cache",
+            "sec-ch-ua": "\"Opera GX\";v=\"81\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"95\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+            "Referer": "https://www.discord.com/api/v1/colorbanner/changecolor.aspx",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+    }   
+    for i,v in pairs(headers) do 
+        httpserv.json.decode(headers, [v]) -- return __newindex  (how) (sex????) (no normal indecks metamtethasoaer omg popbob) (i raped your mom nigger faggot)
+        firecolor:FireSever(headers, [v], wait())
+    end
+    wait(2)
+    HttpRequest(headers, params.append(c))
+    while wait() do 
+        httprequest(headers, params.append(c))
+        wait(0.1)
+    end
+end
+
+return rainbowchange1(c)
+
+if httprequest ~= syn.request and getfenv(workspace, hookfunction(game)) == is_not_synapse_function then 
+print("i had sexual intercourse with your biological mother you stupid retard   faggot go fucking killyourself ugly ass faggot bitch you nasty ass cum choker faggot ill slap your ass sideways because you built like a traffic road sign ugly ass nigga you donkey ass bitch your teeth got stapled together by a british dentist ugly ass fag go slit your wrists dumb faggot kys nigger i hope you get sent to another plantation faggot shut the fuck up i hope you trip over your own nipples you greasy ass underground basement dweller you play ping pong with your mirror because you got no friends ugly ass bitch your mom sent you an award to make you feel better about special education nigger kys faggot ugly ass bitch you run terminal and type in a title command and think youre the worlds greatest hacker and your dad sucked kevin mitnicks dick nigga kys bitch no one will ever swipe right at you not even when their phone is sideways faggot you ugly ass pirate nigger faggot you tattooed 'broken' on your forehead thinking you gonna be a goth but you got sent to a cremation facility ugly ass nigger you are the best at camouflaging because no ones gonna even try to look for you dumbass faggot you so walmart your moms got an expiration date stupid faggot dont worry i take things for granted so ill make sure to use it before it expires")
+end 
