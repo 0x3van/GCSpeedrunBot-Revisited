1e212d5446fb48a2f755980c9ef47fcad8442224
evannnns <alt1@trickster.pw>
2022-04-20 20:12:26 +0800

    complete overhaul of the script, using a new library and more

---
+++ src/dev.lua
@@ -0,0 +1,118 @@
+local httprequests = require('httprequests')
+local GoogleClassroom = require('GoogleClassroomAPI')
+local time = GoogleClassroom:Assignments("os.difftime()")
+local jsontable = {}
+local json = require('json')
+
+for i,v in ipairs(GoogleClassroom.Comments) do 
+    if v[#-1] = math.sin(time, comment) ... math.sub(v.__index) ... then 
+        return comment
+    end
+end
+if time == 'os.difftime(S>M)' then 
+    tostring(time) * 60 * 60
+end
+headers = {
+    ["content-type"] = "application/json"
+    ["content-length"] = "application/json"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+}
+local comment = "[GCSpeedrunBot Revisited],  Completed in " ... time ... "."
+-- GoogleClassroomAPI by Argence, credits to him
+
+-- config
+local http = httprequests.new()
+local OAuthToken = "" -- OAuthToken here
+local ClassroomID = "" -- ClassroomID here
+local ClassroomName = "" -- ClassroomName here
+local ClassroomURL = "" -- ClassroomURL here
+
+function GetClassroom()
+    local Classroom = GoogleClassroom:GetClassroom(ClassroomID, OAuthToken)
+    return Classroom
+end
+
+function GetClassroomName()
+    local Classroom = GetClassroom()
+    local ClassroomName = Classroom.Name
+    return ClassroomName
+end
+
+function GetClassroomURL()
+    local Classroom = GetClassroom()
+    local ClassroomURL = Classroom.CourseWork.CourseMaterial.DriveFile.WebViewLink
+    return ClassroomURL
+end
+
+function GetClassroomID()
+    local Classroom = GetClassroom()
+    local ClassroomID = Classroom.Id
+    return ClassroomID
+end
+
+function main()
+    GetClassroomID()
+    GetClassroomName()
+    GetClassroomURL()
+    if GoogleClassroom.OpenAssignment(ClassroomID, OAuthToken) then
+        print("Successfully opened assignment. Timer starting")
+        timer = function (time)
+            local init = os.time()
+            local diff=os.difftime(os.time(),init)
+            while diff<time do
+                coroutine.yield(diff)
+                diff=os.difftime(os.time(),init)
+            end
+            print( 'Timer timed out at '..time..' seconds!')
+            io.read()
+        end
+    else
+        print("Failed to open assignment")
+    end
+until GoogleClassroom.OpenAssignment == false and GoogleClassroom.API.Embed.Submit() = true then 
+    timer = coroutine.yield()
+    return timer.gsub(comment, time)
+    headers.encode(jsontable)
+    wait(2)
+    table.sort(jsontable)
+    wait(3)
+    table.insert(jsontable, "."... time ...)
+    http:post(ClassroomURL, comment, headers=jsontable[#2] * math.add(jsontable[#1] - tostring(headers)))
+end
+
+function table()
+    local function table.sort(t)
+        local keys = {}
+        for k in pairs(t) do
+            table.insert(keys, k)
+        end
+        table.sort(keys, function(a, b)
+            return t[a] < t[b]
+        end)
+        local i = 0
+        return function()
+            i = i + 1
+            if keys[i] then
+                return keys[i], t[keys[i]]
+            end
+        end
+    end
+end
+
+
+-- call back the comment function script
+function wait(seconds)
+    local start = os.time()
+    repeat until os.time() > start + seconds
+end
+
+callback = function(comment)
+    print(comment)
+end
+return main
+return table
+table()
+main()
+
+
+    
