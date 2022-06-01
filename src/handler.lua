486fdb214ca552669793cb88954de0ba6b59d92c
evan <97516411+0x3van@users.noreply.github.com>
2022-06-01 01:29:14 +0000

    wrote quick handler to append user info for better commenting

---
+++ src/handler.lua
@@ -0,0 +1,16 @@
+local http = require('socket.http')
+local dev = require('dev')
+local ltn12 = require('ltn12')
+for _,v in next, callback(comment) do 
+  wait(2)
+  table.concat(dev.callback2[1])
+end
+
+local call = io.read()
+
+wait(2)
+
+io.write(call, "answer") 
+call.append(#callback2[math.sin(-1, +1)])
+
+
